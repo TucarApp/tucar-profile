9ca035bb98dbf150299af988338a880ec7d4dc41
Marcelo Miranda <78736175+ticoxz@users.noreply.github.com>
2024-11-19 14:29:48 -0300

    rediret

--- helpers/buildUri.js
+++ helpers/buildUri.js
@@ -22,7 +22,7 @@ const buildRequestParams = (params) =>
 
 const buildUri = () => {
     if (MODE !== 'development') return PROFILE_APP_URL;
-    return 'http://localhost:3000';
+    return 'https://tucar-profile-1032838122231.us-central1.run.app';
 };
 
 const baseParams = {
