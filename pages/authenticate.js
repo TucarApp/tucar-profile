5d7cf0f5a51cfcb884d5aac567575b94d9bda62b
Marcelo Miranda <78736175+ticoxz@users.noreply.github.com>
2024-11-26 15:58:19 -0300

    fix env dev

--- pages/authenticate.js
+++ pages/authenticate.js
@@ -83,7 +83,7 @@ const Cuenta = () => {
                     code,
                     client_id: 'E793Gjcib6yVnNpTFD0Hr3jP-Yp6gN04yzTeXGsjlgk',
                     grant_type: 'authorization_code',
-                    redirect_uri: 'https://profile.tucar.app/authenticate',
+                    redirect_uri: 'https://profile.tucar.dev/authenticate',
                     //dev
                     // redirect_uri: 'http://localhost:3000/authenticate',
                 });
